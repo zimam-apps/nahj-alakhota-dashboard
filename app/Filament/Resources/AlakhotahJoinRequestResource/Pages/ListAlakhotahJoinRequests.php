fd54048fbf5238e02921163c8ffdf480359f3512
Muayad <mjasaada@gmail.com>
2025-06-19 14:07:17 +0300

    Add admin panel users module + export for join requests

--- app/Filament/Resources/AlakhotahJoinRequestResource/Pages/ListAlakhotahJoinRequests.php
+++ app/Filament/Resources/AlakhotahJoinRequestResource/Pages/ListAlakhotahJoinRequests.php
@@ -3,13 +3,10 @@
 namespace App\Filament\Resources\AlakhotahJoinRequestResource\Pages;
 
 use App\Filament\Resources\AlakhotahJoinRequestResource;
-use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Actions\Exports\Models\Export;
-use Filament\Actions\Exports\Enums\ExportFormat;
 use pxlrbt\FilamentExcel\Actions\Pages\ExportAction;
+use pxlrbt\FilamentExcel\Columns\Column;
 use pxlrbt\FilamentExcel\Exports\ExcelExport;
-use Illuminate\Support\Facades\Storage;
 
 class ListAlakhotahJoinRequests extends ListRecords
 {
@@ -18,11 +15,15 @@ class ListAlakhotahJoinRequests extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-         ExportAction::make()->exports([
-             ExcelExport::make('form')->fromForm()->withColumns([
-                 \pxlrbt\FilamentExcel\Columns\Column::make('personal_id_image')
+         ExportAction::make()
+	         ->label(__('تصدير'))
+	         ->exports([
+             ExcelExport::make('form')
+	             ->fromForm()
+	             ->withColumns([
+                 Column::make('personal_id_image')
                      ->formatStateUsing(fn ($record) => $record->personal_id_image ? \Storage::disk('digitalocean')->url($record->personal_id_image) : null),
-                 \pxlrbt\FilamentExcel\Columns\Column::make('cv_file')
+                 Column::make('cv_file')
                      ->formatStateUsing(fn ($record) => $record->cv_file ? \Storage::disk('digitalocean')->url($record->cv_file) : null),
              ]),
          ])
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,11 @@
 <?php
 
+use Maatwebsite\Excel\ExcelServiceProvider;
+
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\HorizonServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
+	ExcelServiceProvider::class,
 ];
